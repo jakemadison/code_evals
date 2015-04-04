249a6314ddae05886b25193934d975bd0845f878
jakemadison <jakemadison@gmail.com>
2015-04-04 11:49:46 -0700

    fizzbuzz done! woo hoo! look at me Personal Home Page.

--- code_eval_php/fizzbuzz.php
+++ code_eval_php/fizzbuzz.php
@@ -6,15 +6,43 @@
  * Time: 10:11 AM
  */
 
-$input_file = fopen($argv[1], "r");
-//echo fread($input_file, filesize($argv[1]));
-$fizz_array = explode(" ", fread($input_file, filesize($argv[1])));
 
-foreach ($fizz_array as $f) {
-    echo "each: $f\n";
+function process_line($l, $f_div, $b_div) {
 
+    $fizz_array = explode(" ", $l);
+    $f_div = $fizz_array[0];
+    $b_div = $fizz_array[1];
+    $range_val = $fizz_array[2];
+    $output_array = array();
+
+    foreach (range(1, $range_val) as $r) {
+
+        if ($r % $f_div == 0 and $r % $b_div == 0) {
+            $output_array[] = 'FB';
+        }
+        elseif ($r % $f_div == 0) {
+            $output_array[] = 'F';
+        }
+        elseif ($r % $b_div == 0) {
+            $output_array[] = 'B';
+        }
+        else {
+            $output_array[] = $r;
+        }
+    }
+    return $output_array;
 }
 
+
+$input_file = fopen($argv[1], "r");
+$line_array = explode("\n", fread($input_file, filesize($argv[1])));
 fclose($input_file);
-echo "\n all done!";
+
+foreach ($line_array as $line) {
+
+    $result = process_line($line);
+    echo implode(' ', $result);
+    echo "\n";
+
+}
 
