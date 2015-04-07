31cd76361c01f183b214ccf0e28689a89f43f782
jakemadison <jakemadison@gmail.com>
2015-04-07 07:36:35 -0700

    sum of numbers done.

--- code_eval_php/sumofnumbers.php
+++ code_eval_php/sumofnumbers.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: Madison
- * Date: 15-04-06
- * Time: 6:37 PM
+ * Date: 15-04-07
+ * Time: 7:21 AM
  */
 
 $input_file = fopen($argv[1], "r");
@@ -11,6 +11,7 @@ $line_array = explode("\n", fread($input_file, filesize($argv[1])));
 fclose($input_file);
 
 foreach ($line_array as $line) {
-    print strtolower($line);
+    $line_sum = array_sum(str_split($line));
+    echo $line_sum;
     echo "\n";
 }
